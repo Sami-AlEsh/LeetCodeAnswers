7f40a0dbc103928e051d8d225085e8d0bb94c87a
Sami Al-Esh <42234447+Sami-AlEsh@users.noreply.github.com>
2022-07-20 10:11:08 +0400

    Time: 154 ms (46.05%), Space: 14.2 MB (68.68%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 class Solution(object):
     def twoSum(self, numbers, target):
         """
@@ -5,11 +7,16 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        d = dict()
-        for i, num in enumerate(numbers):
-            if num not in d: d[num] = list()
-            d[num].append(i+1)
-        
-        for n in numbers:
-            if target - n in d:
-                return [d[n].pop(0), d[target-n].pop(0)]
+        start, end = 0, len(numbers)-1
+        while start <= end:
+            mid = ( start + end ) // 2
+            pairs = numbers[start] + numbers[end]
+            
+            if target == pairs:
+                return [start+1, end+1]
+            
+            elif target > pairs:
+                start = mid + 1 if numbers[mid] + numbers[end] < target else start + 1
+            
+            else:
+                end = mid - 1 if numbers[start] + numbers[mid] > target else end - 1
