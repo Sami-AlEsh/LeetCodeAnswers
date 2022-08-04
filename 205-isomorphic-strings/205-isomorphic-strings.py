725b35ac0c0a4362c5e8fd9d7d50026c2c39f6e6
Sami Al-Esh <42234447+Sami-AlEsh@users.noreply.github.com>
2022-08-05 00:29:33 +0400

    Time: 56 ms (69.83%), Space: 14.2 MB (45.52%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -5,6 +5,5 @@ class Solution:
         for i, c in enumerate(s):
             d1[c] = d1.get(c, t[i])
             d2[t[i]] = d2.get(t[i], c)
-
             if d1[c] != t[i] or d2[t[i]] != c: return False
         return True
