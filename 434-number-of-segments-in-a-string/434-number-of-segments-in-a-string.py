42672d3f3abd830bf5b2790d10b4d626105bf93e
Sami AlEsh <42234447+Sami-AlEsh@users.noreply.github.com>
2022-09-11 02:24:04 +0400

    Time: 35 ms (83.87%), Space: 13.8 MB (49.42%) - LeetHub

--- 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.py
+++ 434-number-of-segments-in-a-string/434-number-of-segments-in-a-string.py
@@ -1,4 +1,17 @@
 class Solution:
     def countSegments(self, s: str) -> int:
-        s = list(filter(lambda x: x.strip() != "", s.split(' ')))
-        return len(s) if s != "" else 0
+        if s == "": return 0
+        
+        no_space = True if s[0] != ' ' else False
+        cnt = 0
+        for i, c in enumerate(s):
+            if no_space and c == ' ':
+                no_space = not no_space
+                cnt += 1
+            elif not no_space and c != ' ':
+                no_space = not no_space
+        return cnt + 1 if s[-1] != ' ' else cnt
+    
+        # Easier (slower) solution
+        #s = list(filter(lambda x: x.strip() != "", s.split(' ')))
+        #return len(s) if s != "" else 0
