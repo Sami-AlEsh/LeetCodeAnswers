22cd14b31759f2affcbedacc321c02ce02258be1
Sami AlEsh <42234447+Sami-AlEsh@users.noreply.github.com>
2022-09-18 14:31:32 +0400

    Time: 216 ms (23.05%), Space: 15.6 MB (83.68%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -3,17 +3,19 @@ from collections import deque
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        dp = [[0 for _ in range(n)]  for _ in range(m)]
+        #dp = [[0 for _ in range(n)]  for _ in range(m)]
         
         for i in range(m):
             for j in range(n):
-                dp[i][j] = grid[i][j]
+                #dp[i][j] = grid[i][j]
                 
                 if i == 0 and j == 0: continue
-                
-                s = set()
-                if j > 0: s.add(dp[i][j-1])
-                if i > 0: s.add(dp[i-1][j])
                     
-                dp[i][j] += min(s)
-        return dp[-1][-1]
+                if i > 0 and j > 0:
+                    grid[i][j] += min(grid[i][j-1], grid[i-1][j])
+                elif i > 0:
+                    grid[i][j] += grid[i-1][j]
+                else:
+                    grid[i][j] += grid[i][j-1]
+        
+        return grid[-1][-1]
