1a808d8f78cdb9e30eb39292aa56eab1ed1c3f9c
Sami AlEsh <42234447+Sami-AlEsh@users.noreply.github.com>
2022-10-23 23:37:34 +0400

    Time: 66 ms (21.23%), Space: 14 MB (23.93%) - LeetHub

---
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -0,0 +1,33 @@
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+        index1 = len(s) - 1
+        index2 = len(t) - 1
+        while (index1 >= 0 or index2 >= 0):
+            i1 = self.get_next_valid_char_index(s, index1)
+            i2 = self.get_next_valid_char_index(t, index2)
+            if i1 < 0 and i2 < 0:  # reached the end of both the strings
+                return True
+            if i1 < 0 or i2 < 0:  # reached the end of one of the strings
+                return False
+            if s[i1] != t[i2]:  # check if the characters are equal
+                return False
+
+            index1 = i1 - 1
+            index2 = i2 - 1
+
+        return True
+    
+    def get_next_valid_char_index(self, s, index):
+        backspace_count = 0
+        while (index >= 0):
+            if s[index] == '#':  # found a backspace character
+                backspace_count += 1
+            elif backspace_count > 0:  # a non-backspace character
+                backspace_count -= 1
+            else:
+                break
+
+            index -= 1  # skip a backspace or a valid character
+
+        return index
+
