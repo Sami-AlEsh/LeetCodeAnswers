594d9fecb3bc7e15e7986c621c7f63a3c44d5563
Sami AlEsh <42234447+Sami-AlEsh@users.noreply.github.com>
2022-10-30 23:42:32 +0400

    Time: 313 ms (57.20%), Space: 18.9 MB (27.25%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -9,14 +9,13 @@ class Solution:
         mergedIntervals = []
         start = intervals[0][0]
         end = intervals[0][1]
-        for i in range(1, len(intervals)):
-            interval = intervals[i]
-            if interval[0] <= end:  # overlapping intervals, adjust the 'end'
-                end = max(interval[1], end)
-            else:  # non-overlapping interval, add the previous internval and reset
+        
+        for interval in intervals[1:]:
+            if end >= interval[0]:  # overlapping intervals
+                end = max(end, interval[1])
+            else:  # non-overlapping interval
                 mergedIntervals.append([start, end])
-                start = interval[0]
-                end = interval[1]
+                start, end = interval[0], interval[1]
 
         # add the last interval
         mergedIntervals.append([start, end])
