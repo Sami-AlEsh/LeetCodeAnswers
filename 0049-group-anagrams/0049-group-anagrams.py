34a6b3439e23f3214c5484448ee6c9f1bfb61725
Sami AlEsh <42234447+Sami-AlEsh@users.noreply.github.com>
2022-10-31 12:31:23 +0400

    Time: 235 ms (42.82%), Space: 17.2 MB (78.09%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,16 +1,9 @@
 from collections import defaultdict, Counter
 
 class Solution:
-    def hash(self, s):
-        result = [0] * 26
-        d = Counter(s)
-        for c in d.keys():
-            result[ord(c)-ord('a')] = d[c]
-        return str(result)
-    
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = defaultdict(lambda: list())
         for s in strs:
-            d[self.hash(s)].append(s)
+            d[''.join(sorted(s))].append(s)
 
         return list(d.values())
