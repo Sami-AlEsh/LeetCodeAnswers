719ec8f1551ebe4b267a2aacb0007040466fe7a1
Sami AlEsh <42234447+Sami-AlEsh@users.noreply.github.com>
2022-11-10 03:38:55 +0400

    Time: 154 ms (94.32%), Space: 14.9 MB (49.70%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.py
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -4,10 +4,8 @@ class Solution:
         result = []
         
         while i < len(firstList) and j < len(secondList):
-            a_in_b = firstList[i][0] >= secondList[j][0] and firstList[i][0] <= secondList[j][1]
-            b_in_a = secondList[j][0] >= firstList[i][0] and secondList[j][0] <= firstList[i][1]
-            
-            if a_in_b or b_in_a:
+            # Criss-cross lock
+            if firstList[i][0] <= secondList[j][1] and firstList[i][1] >= secondList[j][0]:
                 result.append([max(firstList[i][0], secondList[j][0]), min(firstList[i][1], secondList[j][1])])
             
             if firstList[i][1] < secondList[j][1]:
